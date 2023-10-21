15f78039c692ced959a942ca8ecbe2d4e74cb10f
JMiguelsilva2003 <jmigueldasilva953@gmail.com>
2023-10-20 21:41:43 -0300

    at 1

--- src/main/App.java
+++ src/main/App.java
@@ -1,12 +1,26 @@
 package main;
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
-        int a = 0;
-        return -1;
+
+        for (int i = 0; i < arrayOrdenado.length; i++) {
+            if (arrayOrdenado[i] == num) {
+                return 1;
+            }
+        }
+        return 0;
     }
 
     public static int maior(int[] arrayDesordenado){
-        return -1;
+
+        int num2 = -99999999;
+        for (int i = 0; i < arrayDesordenado.length; i++) {
+           int num = arrayDesordenado[i];
+           if (num2 < num){
+            num2 = num;
+
+           }
+        }
+        return num2;
     }
 
     public static Integer parMaisProximo(Integer[][] pares){
